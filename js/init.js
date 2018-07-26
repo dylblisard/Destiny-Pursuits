b318ee62323fd9ac43e8d4112423d0989f8fb5a3
dyl.blisard <38386939+dylblisard@users.noreply.github.com>
2018-07-26 02:12:57 -0700

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,20 @@
+
+//Modal
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+
+//Collapsible
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+//Navbar
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+//Carousel
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
